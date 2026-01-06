e1457d9f2c381050586c7041599c2759c75b463a
Jhonatan Amórtegui <yonandres97@gmail.com>
2026-01-06 17:41:34 -0500

    se ajustó la animación de la portada del juego para que se viera mejor (los puntos viajan con aproximadamente la misma pendiente)

--- app/src/main/java/com/example/questionary/ui/ShootingStarsBackground.kt
+++ app/src/main/java/com/example/questionary/ui/ShootingStarsBackground.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.sqrt
 import kotlin.random.Random
 
 @Composable
@@ -45,13 +46,34 @@ fun ShootingStar(
     val alpha = remember { Animatable(0f) }
 
     LaunchedEffect(Unit) {
+        val baseSlope = 1.5
         while(true){
             delay(delayMillis)
-            val endX = Random.nextFloat() * 1000f + 500f
-            val endY = endX * 1.75f//+ Random.nextFloat()*650f // diagonal
-            val initY = Random.nextFloat() * endY
-            val initX = if(initY > endY*0.1) 0f
-                else Random.nextFloat() * endX
+
+            // El grado de ruido (incertidumbre) en la pendiente de la línea
+            val noise = (Random.nextFloat() * 2f - 1f) * 0.15f
+            val m = baseSlope + noise
+            // Normalización
+            val norm = sqrt(1 + m*m)
+            // Longitud controlada (1500 -> 2000)
+            val longitude = 1500f + Random.nextFloat() * 500f
+            val dx = longitude / norm
+            val dy = longitude * m / norm
+            // 65% prob. de que x sea 0 (necesitamos más puntos en x = 0 que en y = 0)
+            val xZeroProb = Random.nextFloat()
+            val initX = if(xZeroProb < 0.65) 0f
+                else xZeroProb * 1100f * Random.nextFloat()
+            val initY = if(initX == 0f) Random.nextFloat() * 2000f
+                else 0f
+            val endX = (initX + dx).toFloat()
+            val endY = (initY + dy).toFloat()
+
+
+            //val endX = Random.nextFloat() * 1100f + 600f
+            //val endY = endX * 1.75f//+ Random.nextFloat()*650f // diagonal
+            //val initY = Random.nextFloat() * endY
+            //val initX = if(initY > endY*0.3) 0f
+            //    else Random.nextFloat() * endX
             xOffset.snapTo(initX)
             yOffset.snapTo(initY)
             alpha.snapTo(1f)
